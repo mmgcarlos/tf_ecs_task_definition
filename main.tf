170259d80b33985debb073e0323622fa93f6eecb
Neil Haggerty <neil.haggerty@mergermarket.com>
2018-01-31 17:05:10 +0000

    testing awsvpc network mode

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_ecs_task_definition" "taskdef" {
   family                = "${var.family}"
   container_definitions = "[${join(",", var.container_definitions)}]"
   task_role_arn         = "${var.task_role_arn}"
+  network_mode          = "awsvpc"
 
   volume = {
     name      = "${lookup(var.volume, "name", "dummy")}"
