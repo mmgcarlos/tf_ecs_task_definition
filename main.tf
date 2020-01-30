a3860ebf74eb2b23b4212d72e3fcddf40cd5d120
Carlos Solis <carlos.solis@mergermarket.com>
2020-01-30 17:42:25 +0000

    Add network mode parameter

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_ecs_task_definition" "taskdef" {
   container_definitions = "[${join(",", var.container_definitions)}]"
   task_role_arn         = "${var.task_role_arn}"
   execution_role_arn    = "${var.execution_role_arn}"
+  network_mode          = "${var.network_mode}"
 
   volume = {
     name      = "${lookup(var.volume, "name", "dummy")}"
